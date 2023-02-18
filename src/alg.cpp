9a90759a999e4d99fd7e70edb19b896001327d25
Evgeniy Naumov <63817948+evgenii020304@users.noreply.github.com>
2023-02-18 21:38:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -35,7 +35,7 @@ uint64_t nextPrime(uint64_t value) {
     }
 }
 
-uint64_t sumPrime1(uint64_t hbound) {
+uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
     while (hbound != 0) {
         if (checkPrime(hbound)) {
